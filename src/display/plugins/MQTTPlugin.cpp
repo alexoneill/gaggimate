58d298a2e6d18edc4ed4a91054c7e13a9c21a9a5
Alex O'Neill <me@aoneill.me>
2026-09-16 15:45:10 -0400

    feat: Control basic brewing state from MQTT

--- src/display/plugins/MQTTPlugin.cpp
+++ src/display/plugins/MQTTPlugin.cpp
@@ -4,10 +4,25 @@
 #include <ctime>
 #include <esp_log.h>
 
+namespace {
+
 const String LOG_TAG = F("MQTTPlugin");
 
-const int DISCOVERY_FREQUENCY_SEC = 60;
-const int STATE_FREQUENCY_SEC = 5;
+// Produces the HomeAssistant device identifier.
+String deviceId() {
+    String mac = WiFi.macAddress();
+    mac.replace(":", "_");
+    return mac;
+}
+
+// Produces the fully qualified device MQTTT topic name given a base topic name.
+String topicFQN(const std::string &topic) {
+    char fqn[80];
+    snprintf(fqn, sizeof(fqn), "gaggimate/%s/%s", deviceId().c_str(), topic.c_str());
+    return String(fqn);
+}
+
+} // namespace
 
 bool MQTTPlugin::connect(Controller *controller) {
     const Settings settings = controller->getSettings();
@@ -19,11 +34,22 @@ bool MQTTPlugin::connect(Controller *controller) {
 
     client.begin(ip.c_str(), haPort, net);
     client.setKeepAlive(10);
+    client.onMessage(std::bind(&MQTTPlugin::messageReceived, this, std::placeholders::_1, std::placeholders::_2));
+
     ESP_LOGI(LOG_TAG.c_str(), "Connecting to %s:%d", ip.c_str(), haPort);
     for (int i = 0; i < MQTT_CONNECTION_RETRIES; i++) {
         ESP_LOGD(LOG_TAG.c_str(), "Attempt (%d/%d)", i + 1, MQTT_CONNECTION_RETRIES);
         if (client.connect(clientId.c_str(), haUser.c_str(), haPassword.c_str())) {
             ESP_LOGI(LOG_TAG.c_str(), "Successfully connected");
+
+            // Subscribe for control messages.
+            client.subscribe(topicFQN("controller/mode/set"));
+            client.subscribe(topicFQN("controller/profile/set"));
+            client.subscribe(topicFQN("controller/brew_button/trigger"));
+
+            ESP_LOGI(LOG_TAG.c_str(), "Subscribed to control topics");
+
+            this->exportDiscovery.set(true);
             return true;
         }
         delay(MQTT_CONNECTION_DELAY);
@@ -37,20 +63,17 @@ void MQTTPlugin::publishDiscovery(Controller *controller) {
         return;
     const Settings settings = controller->getSettings();
     const String haTopic = settings.getHomeAssistantTopic();
-    String mac = WiFi.macAddress();
-    mac.replace(":", "_");
-    const char *cmac = mac.c_str();
 
     JsonDocument device;
     JsonDocument origin;
     JsonDocument components;
 
     // Device information
-    device["ids"] = cmac;
+    device["ids"] = deviceId();
     device["name"] = "GaggiMate";
     device["mf"] = "GaggiMate";
     device["mdl"] = "GaggiMate";
-    device["sn"] = cmac;
+    device["sn"] = deviceId();
     device["sw"] = controller->getSystemInfo().version;
     device["hw"] = controller->getSystemInfo().hardware;
 
@@ -64,44 +87,120 @@ void MQTTPlugin::publishDiscovery(Controller *controller) {
     JsonDocument boilerTemperature;
     JsonDocument boilerTargetTemperature;
     JsonDocument mode;
+    JsonDocument profile;
+    JsonDocument brewSwitch;
+    JsonDocument steamSwitch;
 
+    // Boiler temperature component.
     boilerTemperature["name"] = "Boiler Temperature";
     boilerTemperature["p"] = "sensor";
     boilerTemperature["device_class"] = "temperature";
     boilerTemperature["unit_of_measurement"] = "°C";
-    boilerTemperature["value_template"] = "{{ value_json.temperature | round(2) }}";
+    boilerTemperature["value_template"] = "{{ value | float | round(0) }}";
     boilerTemperature["unique_id"] = "boiler0Tmp";
-    boilerTemperature["state_topic"] = "gaggimate/" + String(cmac) + "/boilers/0/temperature";
+    boilerTemperature["state_topic"] = topicFQN("boilers/0/temperature/state");
 
+    // Boiler target temperature component.
     boilerTargetTemperature["name"] = "Boiler Target Temperature";
     boilerTargetTemperature["p"] = "sensor";
     boilerTargetTemperature["device_class"] = "temperature";
     boilerTargetTemperature["unit_of_measurement"] = "°C";
-    boilerTargetTemperature["value_template"] = "{{ value_json.temperature | round(2) }}";
+    boilerTargetTemperature["value_template"] = "{{ value | float | round(0) }}";
     boilerTargetTemperature["unique_id"] = "boiler0TargetTmp";
-    boilerTargetTemperature["state_topic"] = "gaggimate/" + String(cmac) + "/boilers/0/targetTemperature";
+    boilerTargetTemperature["state_topic"] = topicFQN("boilers/0/targetTemperature/state");
 
+    // Mode select.
     mode["name"] = "Mode";
-    mode["p"] = "text";
-    mode["device_class"] = "text";
-    mode["value_template"] = "{{ value_json.mode_str }}";
+    mode["p"] = "select";
     mode["unique_id"] = "mode";
-    mode["state_topic"] = "gaggimate/" + String(cmac) + "/controller/mode";
+    mode["state_topic"] = topicFQN("controller/mode/state");
+    mode["command_topic"] = topicFQN("controller/mode/set");
+    JsonArray modeOptions = mode["options"].to<JsonArray>();
+    modeOptions.add("Brew");
+    modeOptions.add("Grind");
+    modeOptions.add("Steam");
+    modeOptions.add("Water");
+    modeOptions.add("Standby");
 
+    // Profile select.
+    profile["name"] = "Profile";
+    profile["p"] = "select";
+    profile["unique_id"] = "profile";
+    profile["state_topic"] = topicFQN("controller/profile/state");
+    profile["command_topic"] = topicFQN("controller/profile/set");
+
+    // Gaggiamate allows multiple profiles with the same name, while
+    // HomeAssistant expects unique options for a 'select' component. The
+    // solution is therefore to use the Gaggiamate-assigned unique identifiers
+    // for the profiles to differentiate. However, this is not a great UX as it
+    // may not be easy to know the profile from the unique ID (esp. for the
+    // 'Default' profile).
+    //
+    // To solve this, we use a mapping within the value_template and
+    // command_template to allow the Gaggiamate to communnicate in unique ids
+    // while Home Assistant displays and works with the user-friendly names. We
+    // must construct a mapping on-the-fly and encode that as part of the
+    // templates below.
+    String profileValueTemplate = "{% set mapper = {";
+    String commandValueTemplate = "{% set mapper = {";
+
+    JsonArray profileOptions = profile["options"].to<JsonArray>();
+    std::vector<String> profiles = controller->getProfileManager()->listProfiles();
+    for (int i = 0; i < profiles.size(); i++) {
+        Profile prof;
+        if (!controller->getProfileManager()->loadProfile(profiles[i], prof)) {
+            // Skip unknown profiles.
+            continue;
+        }
+
+        // Simple escaping to prevent a bad JSON blob.
+        String profileName = prof.label;
+        profileName.replace("'", "_");
+        // Another quirk, Gaggiamate allows two profiles with the same name,
+        // HomeAssistant expects unique keys in the 'select' component. We can
+        // simply add zero-width spaces at the end of the names to ensure that
+        // no two entries are identical.
+        for (int j = 0; j < i; j++) {
+            profileName += "\u200B";
+        }
+
+        // Make sure we are separating consecutive entries in the mapping.
+        if (i > 0) {
+            profileValueTemplate += ",";
+            commandValueTemplate += ",";
+        }
+
+        profileValueTemplate += "'" + profiles[i] + "': '" + profileName + "'";
+        commandValueTemplate += "'" + profileName + "': '" + profiles[i] + "'";
+        profileOptions.add(profileName);
+    }
+    profile["value_template"] = profileValueTemplate + "} %}{{ mapper[value] }}";
+    profile["command_template"] = commandValueTemplate + "} %}{{ mapper[value] }}";
+
+    // Brew momentary switch.
+    //
+    // Matches the behavior of the physical brew switch.
+    brewSwitch["name"] = "Brew";
+    brewSwitch["p"] = "button";
+    brewSwitch["unique_id"] = "brew_button";
+    brewSwitch["command_topic"] = topicFQN("controller/brew_button/trigger");
+
+    // Register all components by their ID.
     cmps["boiler"] = boilerTemperature;
     cmps["boiler_target"] = boilerTargetTemperature;
     cmps["mode"] = mode;
+    cmps["profile"] = profile;
+    cmps["brew_button"] = brewSwitch;
 
     // Prepare the payload for Home Assistant discovery
     JsonDocument payload;
     payload["dev"] = device;
     payload["o"] = origin;
     payload["cmps"] = cmps;
-    payload["state_topic"] = "gaggimate/" + String(cmac) + "/state";
     payload["qos"] = 2;
 
     char publishTopic[80];
-    snprintf(publishTopic, sizeof(publishTopic), "%s/device/%s/config", haTopic.c_str(), cmac);
+    snprintf(publishTopic, sizeof(publishTopic), "%s/device/%s/config", haTopic.c_str(), deviceId().c_str());
 
     String payloadStr;
     serializeJson(payload, payloadStr);
@@ -113,46 +212,31 @@ void MQTTPlugin::publishDiscovery(Controller *controller) {
 void MQTTPlugin::publish(const std::string &topic, const std::string &message) {
     if (!client.connected())
         return;
-    String mac = WiFi.macAddress();
-    mac.replace(":", "_");
-    const char *cmac = mac.c_str();
-    char publishTopic[80];
-    snprintf(publishTopic, sizeof(publishTopic), "gaggimate/%s/%s", cmac, topic.c_str());
 
-    ESP_LOGD(LOG_TAG.c_str(), "Publishing %s: %s", publishTopic, message.c_str());
-    client.publish(publishTopic, message.c_str());
-}
-
-void MQTTPlugin::publishBrewState(const char *state) {
-    char json[100];
-    std::time_t now = std::time(nullptr); // Get current timestame
-    snprintf(json, sizeof(json), R"({"state":"%s","timestamp":%ld})", state, now);
-    publish("controller/brew/state", json);
+    String fqn = topicFQN(topic);
+    ESP_LOGI(LOG_TAG.c_str(), "Publishing %s: %s", fqn.c_str(), message.c_str());
+    client.publish(fqn, message.c_str());
 }
 
 void MQTTPlugin::setup(Controller *controller, PluginManager *pluginManager) {
     this->controller = controller;
-    pluginManager->on("controller:wifi:connect", [this, controller](const Event &) {
-        this->hasWifi = true;
-    });
-    pluginManager->on("controller:wifi:disconnect", [this, controller](const Event &) {
-        this->hasWifi = false;
-    });
+    this->pluginManager = pluginManager;
+    pluginManager->on("controller:wifi:connect", [this](const Event &) { this->hasWifi = true; });
+    pluginManager->on("controller:wifi:disconnect", [this](const Event &) { this->hasWifi = false; });
 
-    pluginManager->on("boiler:currentTemperature:change", [this](Event const &event) {
-        this->currentTemperature = event.getFloat("value");
-    });
-    pluginManager->on("boiler:targetTemperature:change", [this](Event const &event) {
-        this->targetTemperature = event.getFloat("value");
-    });
-    pluginManager->on("controller:mode:change", [this](Event const &event) {
-        this->mode = event.getInt("value");
-    });
-    pluginManager->on("controller:brew:start", [this](Event const &) {
-        this->brewing = true;
-    });
-    pluginManager->on("controller:brew:end", [this](Event const &) {
-        this->brewing = false;
+    pluginManager->on("boiler:currentTemperature:change",
+                      [this](Event const &event) { this->currentTemperature.set(round(event.getFloat("value"))); });
+    pluginManager->on("boiler:targetTemperature:change",
+                      [this](Event const &event) { this->targetTemperature.set(round(event.getFloat("value"))); });
+    pluginManager->on("controller:mode:change", [this](Event const &event) { this->mode.set(event.getInt("value")); });
+    pluginManager->on("profiles:profile:select",
+                      [this](Event const &event) { this->profile.set(event.getString("id").c_str()); });
+
+    // Setup some initial state.
+    pluginManager->on("controller:ready", [this](const Event &) {
+        // Only update profile, mode is changed soon after this event.
+        this->profile.set(this->controller->getProfileManager()->getSelectedProfile().id.c_str());
+        this->isReady = true;
     });
 }
 
@@ -160,78 +244,92 @@ void MQTTPlugin::loop() {
     client.loop();
 
     if (!hasWifi) {
-      return;
+        return;
     }
-
-    std::time_t now = std::time(nullptr); // Get current timestamp
-
-    bool updateDiscovery = false;
-    if (now - lastDiscoveryTime >= DISCOVERY_FREQUENCY_SEC) {
-        updateDiscovery = true;
-        lastDiscoveryTime = now;
-    }
-
-    bool updateState = false;
-    if (now - lastStateTime >= STATE_FREQUENCY_SEC) {
-        updateState = true;
-        lastStateTime = now;
-    }
-
-    if (!updateDiscovery && !updateState) {
+    // If the client has lost connection, reconnect.
+    if (!client.connected() && !connect(controller)) {
         return;
     }
 
-    // If the client has lost connection, reconnect.
-    if (!client.connected()) {
-        if (!connect(controller)) {
-            return;
-        }
-    }
-
-    if (updateDiscovery) {
+    if (this->exportDiscovery.should_report()) {
+        this->exportDiscovery.report(); // ignore value, report below.
         publishDiscovery(controller);
     }
-    if (updateState) {
-        if (currentTemperature.has_value()) {
-            char json[50];
-            snprintf(json, sizeof(json), R"***({"temperature":%02f})***", *currentTemperature);
-            publish("boilers/0/temperature", json);
+    if (this->currentTemperature.should_report()) {
+        String tempStr(this->currentTemperature.report(), 2);
+        publish("boilers/0/temperature/state", tempStr.c_str());
+    }
+    if (this->targetTemperature.should_report()) {
+        String tempStr(this->targetTemperature.report(), 2);
+        publish("boilers/0/targetTemperature/state", tempStr.c_str());
+    }
+    if (this->mode.should_report()) {
+        const char *modeStr = "";
+        // clang-format off
+        switch (this->mode.report()) {
+        case MODE_STANDBY: modeStr = "Standby"; break;
+        case MODE_BREW:    modeStr = "Brew";    break;
+        case MODE_STEAM:   modeStr = "Steam";   break;
+        case MODE_WATER:   modeStr = "Water";   break;
+        case MODE_GRIND:   modeStr = "Grind";   break;
         }
-        if (targetTemperature.has_value()) {
-            char json[50];
-            snprintf(json, sizeof(json), R"***({"temperature":%02f})***", *targetTemperature);
-            publish("boilers/0/targetTemperature", json);
+        // clang-format on
+        publish("controller/mode/state", modeStr);
+    }
+    if (this->profile.should_report()) {
+        publish("controller/profile/state", this->profile.report());
+    }
+}
+
+void MQTTPlugin::messageReceived(String &topic, String &payload) {
+    if (!this->isReady) {
+        return;
+    }
+
+    ESP_LOGI(LOG_TAG.c_str(), "Message received on %s: %s", topic.c_str(), payload.c_str());
+
+    if (topic == topicFQN("controller/mode/set")) {
+        if (payload == "Brew") {
+            controller->deactivate();
+            controller->setMode(MODE_BREW);
+        } else if (payload == "Grind") {
+            controller->setMode(MODE_GRIND);
+            controller->deactivate();
+        } else if (payload == "Standby") {
+            controller->activateStandby();
+        } else if (payload == "Steam") {
+            controller->setMode(MODE_STEAM);
+            controller->deactivate();
+        } else if (payload == "Water") {
+            controller->setMode(MODE_WATER);
+            controller->deactivate();
+        } else {
+            ESP_LOGW(LOG_TAG, "Payload on %s unsupported: %s", topic.c_str(), payload.c_str());
         }
-        if (mode.has_value()) {
-            const char *modeStr;
-            switch (*mode) {
-            case 0:
-                modeStr = "Standby";
-                break;
-            case 1:
-                modeStr = "Brew";
-                break;
-            case 2:
-                modeStr = "Steam";
-                break;
-            case 3:
-                modeStr = "Water";
-                break;
-            case 4:
-                modeStr = "Grind";
-                break;
-            default:
-                modeStr = "Unknown";
-                break; // Fallback in case of unexpected value
+        return;
+    }
+
+    if (topic == topicFQN("controller/profile/set")) {
+        if (controller->getProfileManager()->profileExists(payload)) {
+            controller->getProfileManager()->selectProfile(payload);
+        } else {
+            ESP_LOGW(LOG_TAG, "Payload on %s unsupported: %s", topic.c_str(), payload.c_str());
+        }
+        return;
+    }
+
+    if (topic == topicFQN("controller/brew_button/trigger")) {
+        if (controller->getMode() == MODE_BREW) {
+            if (!controller->isActive()) {
+                controller->activate();
+            } else {
+                controller->deactivate();
+                controller->clear();
             }
-            char json[100];
-            snprintf(json, sizeof(json), R"({"mode":%d,"mode_str":"%s"})", *mode, modeStr);
-            publish("controller/mode", json);
+        } else {
+            controller->deactivate();
+            controller->setMode(MODE_BREW);
         }
-        if (brewing.has_value()) {
-            char json[100];
-            snprintf(json, sizeof(json), R"({"state":"%s","timestamp":%ld})", *brewing ? "brewing" : "not brewing", now);
-            publish("controller/brew/state", json);
-        }
+        return;
     }
 }
--- src/display/plugins/MQTTPlugin.h
+++ src/display/plugins/MQTTPlugin.h
@@ -7,6 +7,57 @@
 
 constexpr int MQTT_CONNECTION_RETRIES = 5;
 constexpr int MQTT_CONNECTION_DELAY = 1000;
+constexpr int MQTT_DISCOVERY_STALE_SEC = 60;
+constexpr int MQTT_STATE_STALE_SEC = 15;
+
+namespace impl { // Internal, not part of the API.
+
+// Helper class to track how often a reportable value changes and trigger
+// reporting of that value. The goal is to report the value when either:
+//   * Enough time has elapsed since the last time the value was reported
+//   * The value itself changes
+template <typename T> class ReportableData {
+  public:
+    explicit ReportableData(int max_report_interval_sec) { this->max_report_interval_sec = max_report_interval_sec; }
+
+    // Setting the value should trigger a report event, unless the set data
+    // remains the same.
+    void set(T data) {
+        // Only compare against data in the container if we have set it before.
+        if (this->reported.has_value() && data == this->data) {
+            return;
+        }
+
+        this->data = data;
+        this->stale = true;
+        this->reported = std::time(nullptr); // Get current timestamp
+    }
+
+    // Report the data, resetting the trackers. The user should take the result
+    // and report it somewhere.
+    T report() {
+        this->stale = false;
+        this->reported = std::time(nullptr); // Get current timestamp
+        return this->data;
+    }
+
+    // If the client should call report().
+    bool should_report() {
+        std::time_t now = std::time(nullptr); // Get current timestamp
+        return this->stale || (this->reported.has_value() && now - *this->reported > this->max_report_interval_sec);
+    }
+
+  private:
+    // Ctor args.
+    int max_report_interval_sec;
+
+    // Tracking report state.
+    bool stale = false;
+    std::optional<std::time_t> reported = std::nullopt;
+    T data;
+};
+
+} // namespace impl
 
 class MQTTPlugin : public Plugin {
   public:
@@ -16,22 +67,24 @@ class MQTTPlugin : public Plugin {
 
   private:
     void publish(const std::string &topic, const std::string &message);
-    void publishBrewState(const char *state);
     void publishDiscovery(Controller *controller);
+    void messageReceived(String &topic, String &payload);
+
     Controller *controller = nullptr;
+    PluginManager *pluginManager = nullptr;
     MQTTClient client;
     WiFiClient net;
 
     // State controlling updates.
     bool hasWifi = false;
-    std::time_t lastDiscoveryTime = 0;
-    std::time_t lastStateTime = 0;
+    bool isReady = false;
 
     // State periodically exported.
-    std::optional<float> currentTemperature = std::nullopt;
-    std::optional<float> targetTemperature = std::nullopt;
-    std::optional<int> mode = std::nullopt;
-    std::optional<bool> brewing = std::nullopt;
+    impl::ReportableData<bool> exportDiscovery{MQTT_DISCOVERY_STALE_SEC};
+    impl::ReportableData<float> currentTemperature{MQTT_STATE_STALE_SEC};
+    impl::ReportableData<float> targetTemperature{MQTT_STATE_STALE_SEC};
+    impl::ReportableData<int> mode{MQTT_STATE_STALE_SEC};
+    impl::ReportableData<std::string> profile{MQTT_STATE_STALE_SEC};
 };
 
 #endif // MQTTPLUGIN_H
